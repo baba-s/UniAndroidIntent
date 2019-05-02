f037812d52818da3dc5b6f8e88a1fb9c28653525
baba_s <shotababa1206@gmail.com>
2019-05-02 17:46:20 +0900

    first commit

---
+++ app/Assets/UniAndroidIntent/Example/Example.cs
@@ -0,0 +1,61 @@
+﻿using KoganeUnityLib;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KoganeUnityLibExample
+{
+	public sealed class Example : MonoBehaviour
+	{
+		[SerializeField] private Text m_text = null;
+
+		private void Start()
+		{
+			var sb = new StringBuilder();
+
+			sb.AppendLine( "---------- int ----------" );
+			sb.AppendLine();
+			sb.AppendLine( UniAndroidIntent.GetInt( "i" ).ToString() );
+			sb.AppendLine();
+			sb.AppendLine( "---------- long ----------" );
+			sb.AppendLine();
+			sb.AppendLine( UniAndroidIntent.GetLong( "l" ).ToString() );
+			sb.AppendLine();
+			sb.AppendLine( "---------- string ----------" );
+			sb.AppendLine();
+			sb.AppendLine( UniAndroidIntent.GetString( "s" ) );
+			sb.AppendLine();
+			sb.AppendLine( "---------- bool ----------" );
+			sb.AppendLine();
+			sb.AppendLine( UniAndroidIntent.GetBool( "b" ).ToString() );
+			sb.AppendLine();
+			sb.AppendLine( "---------- int array ----------" );
+			sb.AppendLine();
+
+			foreach ( var n in UniAndroidIntent.GetIntArray( "ia" ) )
+			{
+				sb.AppendLine( n.ToString() );
+			}
+
+			sb.AppendLine();
+			sb.AppendLine( "---------- long array ----------" );
+			sb.AppendLine();
+
+			foreach ( var n in UniAndroidIntent.GetLongArray( "la" ) )
+			{
+				sb.AppendLine( n.ToString() );
+			}
+
+			sb.AppendLine();
+			sb.AppendLine( "---------- string array ----------" );
+			sb.AppendLine();
+
+			foreach ( var n in UniAndroidIntent.GetStringArray( "sa" ) )
+			{
+				sb.AppendLine( n );
+			}
+
+			m_text.text = sb.ToString();
+		}
+	}
+}
---
+++ app/Assets/UniAndroidIntent/Scripts/UniAndroidIntent.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+namespace KoganeUnityLib
+{
+	public static class UniAndroidIntent
+	{
+		private static bool IsAndroid    => Application.platform == RuntimePlatform.Android;
+		public static  bool IsNotAndroid => !IsAndroid;
+
+		public static bool GetBool( string name, bool defaultValue = default )
+		{
+			if ( IsNotAndroid ) return defaultValue;
+
+			using ( var intent = GetIntent() )
+			{
+				return intent.Call<bool>( "getBooleanExtra", name, defaultValue );
+			}
+		}
+
+		public static int GetInt( string name, int defaultValue = default )
+		{
+			if ( IsNotAndroid ) return defaultValue;
+
+			using ( var intent = GetIntent() )
+			{
+				return intent.Call<int>( "getIntExtra", name, defaultValue );
+			}
+		}
+
+		public static int[] GetIntArray( string name )
+		{
+			if ( IsNotAndroid ) return new int[0];
+
+			using ( var intent = GetIntent() )
+			{
+				return intent.Call<int[]>( "getIntArrayExtra", name ) ?? new int[0];
+			}
+		}
+
+		public static long GetLong( string name, long defaultValue = default )
+		{
+			if ( IsNotAndroid ) return defaultValue;
+
+			using ( var intent = GetIntent() )
+			{
+				return intent.Call<long>( "getLongExtra", name, defaultValue );
+			}
+		}
+
+		public static long[] GetLongArray( string name )
+		{
+			if ( IsNotAndroid ) return new long[0];
+
+			using ( var intent = GetIntent() )
+			{
+				return intent.Call<long[]>( "getLongArrayExtra", name ) ?? new long[0];
+			}
+		}
+
+		public static string GetString( string name )
+		{
+			if ( IsNotAndroid ) return string.Empty;
+
+			using ( var intent = GetIntent() )
+			{
+				return intent.Call<string>( "getStringExtra", name ) ?? string.Empty;
+			}
+		}
+
+		public static string[] GetStringArray( string name )
+		{
+			if ( IsNotAndroid ) return new string[0];
+
+			using ( var intent = GetIntent() )
+			{
+				return intent.Call<string[]>( "getStringArrayExtra", name ) ?? new string[0];
+			}
+		}
+
+		private static AndroidJavaObject GetIntent()
+		{
+			using ( var player = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" ) )
+			using ( var activity = player.GetStatic<AndroidJavaObject>( "currentActivity" ) )
+			{
+				var intent = activity.Call<AndroidJavaObject>( "getIntent" );
+				return intent;
+			}
+		}
+	}
+}
